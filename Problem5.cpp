c37dd9da3ebda164719e827d8d42eced623fa81f
Honglin Zhang <zhang.t.honglin@gmail.com>
2016-02-21 20:35:54 -0800

    get it working

--- Problem5.cpp
+++ Problem5.cpp
@@ -1,7 +1,16 @@
 // Description
-// The Prince of Persia has been thrown onto the top level of Jaffar's underground labyrinth. The labyrinth consists of h levels strictly on top of each other. Each level is split into m by n areas. Some areas have columns that support ceiling, some areas are free. The Prince can move only to free areas. To move to the level below the Prince can break the floor underneath him and jump down if there is no column underneath. Every move takes the Prince 5 seconds. A Princess is waiting for the Prince at the lowest level. Write a program that will help the Prince to save the Princess as fast as possible by finding the shortest path between them and outputting time it took the Prince to find the Princess. The structure of the labyrinth is given bellow. The Prince’s location is marked with '1', the Princess’s location is marked with '2'. ‘.’ - marks a free spot and ‘o’ marks a column.
-
-
+// The Prince of Persia has been thrown onto the top level of Jaffar's
+// underground labyrinth. The labyrinth consists of h levels strictly on top of
+// each other. Each level is split into m by n areas. Some areas have columns
+// that support ceiling, some areas are free. The Prince can move only to free
+// areas. To move to the level below the Prince can break the floor underneath
+// him and jump down if there is no column underneath. Every move takes the
+// Prince 5 seconds. A Princess is waiting for the Prince at the lowest level.
+// Write a program that will help the Prince to save the Princess as fast as
+// possible by finding the shortest path between them and outputting time it
+// took the Prince to find the Princess. The structure of the labyrinth is given
+// bellow. The Prince’s location is marked with '1', the Princess’s location is
+// marked with '2'. ‘.’ - marks a free spot and ‘o’ marks a column.
 
 /*Given a grid, return a solution object.
 *
@@ -20,162 +29,65 @@
 
 #include <algorithm>
 #include <iostream>
+
 #include "Problem5.hpp"
 
-Problem5::Solution Problem5::savePrincess(const ThreeDimCharArray grid)
+std::shared_ptr<Problem5::Solution> Problem5::savePrincess(const ThreeDimCharArray& grid)
 {
-  Solution solution;
-  solution.amountOfTime = 0u;
-  solution.directions = std::vector<Direction>();
-  solution.isPossible = false;
-
   auto princePosition = getFigurePosition(Figure::PRINCE, grid);
   auto princessPosition = getFigurePosition(Figure::PRINCESS, grid);
-
-  // If princess stands above prince, then it is impossible.
-  // can we jump? It seems impossible based on the description.
-  if (std::get<0>(princessPosition) < std::get<0>(princePosition)) {
-    return solution;
-  }
-
-  auto pool = std::vector<std::tuple<Position, Solution>>();
-  pool.push_back(std::make_tuple(princePosition, solution));
-  auto rejected = std::vector<Position>();
-  auto results = std::vector<Solution>();
-  while (pool.size() != 0)
+  // assume princess is always below prince
+  State state { princePosition };
+  std::vector<State> states = { state };
+  std::vector<State> rejected = {};
+  auto solution = std::make_shared<Solution>();
+  while (!states.empty())
   {
-    auto p = pool.back();
-    pool.pop_back();
-    std::cout << "pool size: " << pool.size() << "\n";
-    std::cout << "rejected size: " << rejected.size() << "\n";
-    // base cases
-    auto currentPrincePosition = std::get<0>(p);
-    auto level = std::get<0>(currentPrincePosition);
-    auto x = std::get<1>(currentPrincePosition);
-    auto y = std::get<2>(currentPrincePosition);
-
-    std::cout << level << ", " << x << ", " << y << "\n";
-
-    auto currentSolution = std::get<1>(p);
-    if ( std::find(rejected.begin(), rejected.end(), currentPrincePosition)
-         != rejected.end() ||
-        level < 0 || level > grid.size() ||
-        x < 0 || x > grid.at(0).size() ||
-        y < 0 || y > grid.at(0).at(0).size() ||
-        grid.at(level).at(x).at(y) == Figure::COLUMN) {
-      std::cout << "rejected" << "\n";
-      rejected.push_back(currentPrincePosition);
+    auto s = states.front();
+    states.erase(states.begin());
+    auto level = std::get<0>(s.position);
+    auto x = std::get<1>(s.position);
+    auto y = std::get<2>(s.position);
+    // base cases; if the position is out of range or at column then reject;
+    // if the position matches the princess', the return;
+    // otherwise at the empty area, safely proceed.
+    if (0 > level || grid.size() == level ||
+        0 > x || grid[0].size() == x      ||
+        0 > y || grid[0][0].size() == y   ||
+        Figure::COLUMN == grid[level][x][y])
+    {
+      rejected.push_back(s);
       continue;
     }
-    if (grid.at(level).at(x).at(y) == Figure::PRINCESS) {
-      std::cout << "got it" << "\n";
-      Solution result;
-      std::memcpy(&result, &currentSolution, sizeof(Solution));
-      result.isPossible = true;
-      results.push_back(result);
-    }
-    // induction; prince is at a free space
-    // try different moves
-    std::cout << "induction" << "\n";
-    auto pp_e = std::make_shared<std::tuple<Position, Solution>>();
-    auto p_e = *pp_e.get();
-    std::memcpy(&p_e, &p, sizeof(std::tuple<Position, Solution>));
-    currentPrincePosition = std::get<0>(p_e);
-    level = std::get<0>(currentPrincePosition);
-    x = std::get<1>(currentPrincePosition);
-    y = std::get<2>(currentPrincePosition);
-    currentPrincePosition = std::make_tuple(level, x, y + 1);
-    currentSolution = std::get<1>(p_e);
-    currentSolution.amountOfTime += 5;
-    currentSolution.directions.push_back(Direction::EAST);
-    p_e = std::make_tuple(currentPrincePosition, currentSolution);
-    pool.push_back(p_e);
-
-    auto pp_w = std::make_shared<std::tuple<Position, Solution>>();
-    auto p_w = *pp_w.get();
-    std::memcpy(&p_w, &p, sizeof(std::tuple<Position, Solution>));
-    currentPrincePosition = std::get<0>(p_w);
-    level = std::get<0>(currentPrincePosition);
-    x = std::get<1>(currentPrincePosition);
-    y = std::get<2>(currentPrincePosition);
-    currentPrincePosition = std::make_tuple(level, x, y - 1);
-    currentSolution = std::get<1>(p_w);
-    currentSolution.amountOfTime += 5;
-    currentSolution.directions.push_back(Direction::WEST);
-    p_w = std::make_tuple(currentPrincePosition, currentSolution);
-    pool.push_back(p_w);
-
-    auto pp_s = std::make_shared<std::tuple<Position, Solution>>();
-    auto p_s = *pp_s.get();
-    std::memcpy(&p_s, &p, sizeof(std::tuple<Position, Solution>));
-    currentPrincePosition = std::get<0>(p_s);
-    level = std::get<0>(currentPrincePosition);
-    x = std::get<1>(currentPrincePosition);
-    y = std::get<2>(currentPrincePosition);
-    currentPrincePosition =  std::make_tuple(level, x + 1, y);
-    currentSolution = std::get<1>(p_s);
-    currentSolution.amountOfTime += 5;
-    currentSolution.directions.push_back(Direction::SOUTH);
-    p_s = std::make_tuple(currentPrincePosition, currentSolution);
-    pool.push_back(p_s);
-
-    auto pp_n = std::make_shared<std::tuple<Position, Solution>>();
-    auto p_n = *pp_n.get();
-    std::memcpy(&p_n, &p, sizeof(std::tuple<Position, Solution>));
-    currentPrincePosition = std::get<0>(p_n);
-    level = std::get<0>(currentPrincePosition);
-    x = std::get<1>(currentPrincePosition);
-    y = std::get<2>(currentPrincePosition);
-    currentPrincePosition =  std::make_tuple(level, x - 1, y);
-    currentSolution = std::get<1>(p_n);
-    currentSolution.amountOfTime += 5;
-    currentSolution.directions.push_back(Direction::NORTH);
-    p_n = std::make_tuple(currentPrincePosition, currentSolution);
-    pool.push_back(p_n);
-
-    auto pp_d = std::make_shared<std::tuple<Position, Solution>>();
-    auto p_d = *pp_d.get();
-    std::memcpy(&p_d, &p, sizeof(std::tuple<Position, Solution>));
-    currentPrincePosition = std::get<0>(p_d);
-    level = std::get<0>(currentPrincePosition);
-    x = std::get<1>(currentPrincePosition);
-    y = std::get<2>(currentPrincePosition);
-    currentPrincePosition =  std::make_tuple(level + 1, x, y);
-    currentSolution = std::get<1>(p_d);
-    currentSolution.amountOfTime += 5;
-    currentSolution.directions.push_back(Direction::DOWN);
-    p_d = std::make_tuple(currentPrincePosition, currentSolution);
-    pool.push_back(p_d);
-  }
-
-  if (results.size() == 0) return solution;
-  return *std::min_element(results.begin(), results.end(), compareBetterSolution);
-}
-
-Problem5::Position Problem5::getFigurePosition(Figure figure,
-                                               const ThreeDimCharArray grid)
-{
-  // assuming grid is well-formed
-  for (auto i = 0; i != grid.size(); ++i)
-  {
-    for (auto j = 0; j != grid.at(0).size(); ++j)
+    if (Figure::PRINCESS == grid[level][x][y])
     {
-      for (auto k = 0; k != grid.at(0).at(0).size(); ++k)
-      {
-        if (grid.at(i).at(j).at(k) == figure)
-        {
-          return std::make_tuple(i, j, k);
-        }
-      }
+      mkSolution(*solution.get(), s.directions);
+      return solution;
     }
+    // induction; try different moves.
+    State east;
+    moveEast(east, s);
+    states.push_back(east);
+    State west;
+    moveWest(west, s);
+    states.push_back(west);
+    State north;
+    moveNorth(north, s);
+    states.push_back(north);
+    State south;
+    moveSouth(south, s);
+    states.push_back(south);
+    State down;
+    moveDown(down, s);
+    states.push_back(down);
   }
-  return std::make_tuple(0, 0, 0);
+  return solution;
 }
 
 int main()
 {
-  //Please implement this, it only returns an empty Solution object.
-  //Please delete this, its just an example of the structure of the grid.
+  // Please implement this, it only returns an empty Solution object.
+  // Please delete this, its just an example of the structure of the grid.
   Problem5::ThreeDimCharArray grid
     {
       {
@@ -195,6 +107,6 @@ int main()
       }
     };
   auto solution = Problem5::savePrincess(grid);
-  std::cout << solution.amountOfTime << "\n";
+  std::cout << solution << "\n";
   return 0;
 }
--- Problem5.hpp
+++ Problem5.hpp
@@ -1,36 +1,158 @@
 #ifndef PROBLEM5_HPP
 #define PROBLEM5_HPP
 
-#include <vector>
+#include <memory>
+#include <ostream>
 #include <tuple>
+#include <unordered_map>
+#include <vector>
 
 namespace Problem5 {
+  bool _DEBUG_ = false;
   // level, x, y
-  using ThreeDimCharArray = std::vector<std::vector<std::vector<char16_t>>>;
+  using ThreeDimCharArray = std::vector<std::vector<std::vector<char>>>;
   // level, x, y
   using Position = std::tuple<int32_t, int32_t, int32_t>;
-
-  int32_t TIME_UNIT = 5;
-
+  uint64_t TIME_UNIT = 5;
   enum Figure
-  { 
-    PRINCE = (char16_t) '1', PRINCESS = (char16_t) '2',
-    SPACE = (char16_t) '.', COLUMN = (char16_t) 'o'
-  };
-  enum Direction { EAST, WEST, SOUTH, NORTH, UP, DOWN };
-
-  struct Solution
   {
+    PRINCE = (char) '1', PRINCESS = (char) '2',
+    SPACE = (char) '.', COLUMN = (char) 'o'
+  };
+  enum Direction { EAST, WEST, SOUTH, NORTH, DOWN };
+  class State
+  {
+  public:
+    Position position;
+    std::vector<Direction> directions;
+  };
+  class Solution
+  {
+  public:
     uint64_t amountOfTime;
     std::vector<Direction> directions;
     bool isPossible;
   };
+  std::shared_ptr<Solution> savePrincess(const ThreeDimCharArray& grid);
 
-  Solution savePrincess(ThreeDimCharArray grid);
-  Position getFigurePosition(Figure figure, ThreeDimCharArray grid);
-  bool compareBetterSolution(const Solution s1, const Solution s2)
+  Position getFigurePosition(const Figure figure, const ThreeDimCharArray& grid)
   {
-    return s1.amountOfTime < s2.amountOfTime;
+    // assuming grid is well-formed
+    for (auto i = 0; i != grid.size(); ++i)
+    {
+      for (auto j = 0; j != grid.at(0).size(); ++j)
+      {
+        for (auto k = 0; k != grid.at(0).at(0).size(); ++k)
+        {
+          if (grid.at(i).at(j).at(k) == figure)
+          {
+            return std::make_tuple(i, j, k);
+          }
+        }
+      }
+    }
+    return std::make_tuple(0, 0, 0);
+  };
+  inline const std::string toString(const Direction& direction)
+  {
+    switch (direction)
+    {
+      case EAST: return "EAST";
+      case WEST: return "WEST";
+      case SOUTH: return "SOUTH";
+      case NORTH: return "NORTH";
+      case DOWN: return "DOWN";
+      default: return "";
+    }
+  };
+  void moveEast(State& dest, const State& src)
+  {
+    auto level = std::get<0>(src.position);
+    auto x = std::get<1>(src.position);
+    auto y = std::get<2>(src.position);
+    auto directions = src.directions;
+    directions.push_back(Direction::EAST);
+    dest = { std::make_tuple(level, x + 1, y), directions };
+  };
+  void moveWest(State& dest, const State& src)
+  {
+    auto level = std::get<0>(src.position);
+    auto x = std::get<1>(src.position);
+    auto y = std::get<2>(src.position);
+    auto directions = src.directions;
+    directions.push_back(Direction::WEST);
+    dest = { std::make_tuple(level, x - 1, y), directions };
+  };
+  void moveNorth(State& dest, const State& src)
+  {
+    auto level = std::get<0>(src.position);
+    auto x = std::get<1>(src.position);
+    auto y = std::get<2>(src.position);
+    auto directions = src.directions;
+    directions.push_back(Direction::NORTH);
+    dest = { std::make_tuple(level, x, y - 1), directions };
+  };
+  void moveSouth(State& dest, const State& src)
+  {
+    auto level = std::get<0>(src.position);
+    auto x = std::get<1>(src.position);
+    auto y = std::get<2>(src.position);
+    auto directions = src.directions;
+    directions.push_back(Direction::SOUTH);
+    dest = { std::make_tuple(level, x, y + 1), directions };
+  };
+  void moveDown(State& dest, const State& src)
+  {
+    auto level = std::get<0>(src.position);
+    auto x = std::get<1>(src.position);
+    auto y = std::get<2>(src.position);
+    auto directions = src.directions;
+    directions.push_back(Direction::DOWN);
+    dest = { std::make_tuple(level + 1, x, y), directions };
+  };
+  void mkSolution(Solution& solution, const std::vector<Direction>& directions)
+  {
+    solution.directions = directions;
+    solution.amountOfTime = directions.size() * TIME_UNIT;
+    solution.isPossible = directions.size() == 0 ? false : true;
+  };
+  std::ostream& operator<<(std::ostream& os, const Position& position)
+  {
+    os << "("
+       << std::get<0>(position) << ", "
+       << std::get<1>(position) << ", "
+       << std::get<2>(position) << "), ";
+    return os;
+  };
+  std::ostream& operator<<(std::ostream& os, const Direction& direction)
+  {
+    return os << toString(direction);
+  }
+  template<typename T>
+  std::ostream& operator<<(std::ostream& os, const std::vector<T>& vector)
+  {
+    os << "[";
+    for (auto&& x : vector)
+    {
+      os << x << ",";
+    }
+    os << "]";
+    return os;
+  };
+  std::ostream& operator<<(std::ostream& os, const State& state)
+  {
+    os << "position: " << state.position << "\n";
+    os << "directions: " << state.directions << "\n";
+    return os;
+  };
+  std::ostream& operator<<(std::ostream& os, const std::shared_ptr<Solution>& solution)
+  {
+    os << "solution: {" << "\n";
+    os << "is possible   : " << solution.get()->isPossible << "\n";
+    os << "amount of time: " << solution.get()->amountOfTime << "\n";
+    os << solution.get()->directions << "\n";
+    os << "}";
+    return os;
   };
 }
 
